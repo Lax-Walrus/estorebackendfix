fbe02c4ad0dea721ae6a101389ff2b48321c9b07
Kevin Lyons <zinckev@gmail.com>
2021-06-02 13:36:47 -0700

    working on upload to heroku again

--- routers/productRouter.js
+++ routers/productRouter.js
@@ -73,7 +73,7 @@ productRouter.get(
 productRouter.get(
   "/seed",
   expressAsyncHandler(async (req, res) => {
-    await Product.remove({});
+    // await Product.remove({});
     const seller = await User.findOne({ isSeller: true });
     if (seller) {
       const products = data.products.map((product) => ({
